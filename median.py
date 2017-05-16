90d7e31dcf7cef58157152749465dba150427fb2
Matthew Mah <mmah@broadinstitute.org>
2017-05-16 12:03:48 -0400

    median for multiple histogram filenames

--- median.py
+++ median.py
@@ -1,6 +1,7 @@
 # read the median from a histogram file produced by org.apache.commons.math3.stat.Frequency
 
 import sys
+import os
 
 def medianFromHistogram(filename):
 	with open(filename, "r") as f:
@@ -23,5 +24,12 @@ def medianFromHistogram(filename):
 				return 0
 	return 0
 
-filename = sys.argv[1]
-print (medianFromHistogram(filename))
+# first argument is label to be aplied to output, for example: media_rsrs
+label = sys.argv[1]
+# iterate over passed files
+filenames = sys.argv[2: len(sys.argv)]
+
+for filename in filenames:
+	key = os.path.splitext(filename)[0] # filename without extension
+	median = medianFromHistogram(filename)
+	print("%s\t%s\t%s" % (key, label, median) )
