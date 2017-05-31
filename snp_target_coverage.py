0e2776f04b279e15e7ea9115742e68b71d7dd616
Matthew Mah <mmah@broadinstitute.org>
2017-05-31 18:00:48 -0400

    python script for spike3k analysis

--- snp_target_coverage.py
+++ snp_target_coverage.py
@@ -1,7 +1,9 @@
 import os
 import sys
-# read in a VCF file
-# this assumes there is at least one read per position reported
+# read in a VCF file and compute:
+# number of reads for autosomal, X, Y chromosomes
+# sex determination using # reads for chromosomes
+# coverage as a fraction of targets
 
 # first argument is label to use
 label = sys.argv[1]
@@ -16,6 +18,10 @@ with open(filename) as f:
 		key = os.path.splitext(key)[0] # filename without extension
 	
 	targets = {}
+	autosome_read_count = 0
+	x_read_count = 0
+	y_read_count = 0
+	
 	for line in f:
 		#ignore lines that start with '#'
 		if line[0] == '#':
@@ -23,12 +29,46 @@ with open(filename) as f:
 		fields = line.split('\t')
 		chromosome = fields[0]
 		position = fields[1]
+		
+		# read depth (number of reads) from info field
+		info = fields[7]
+		info_sections = info.split(';')
+		depth = 0
+		for info_field in info_sections:
+			if info_field.find('DP=') >= 0:
+				depth = int(info_field.replace('DP=',''))
+				break;
+		
 		target_key = chromosome + '-' + position
-		#print(target_key)
+		#print(target_key, depth)
+		# keep track of distinct targets
 		if targets.has_key(target_key):
-			targets[target_key] += 1
+			targets[target_key] += depth
 		else:
-			targets[target_key] = 1
+			targets[target_key] = depth
+			
+		nchromosome = int(chromosome)
+		if 1 <= nchromosome and nchromosome <= 22:
+			autosome_read_count += depth
+		elif nchromosome == 23:
+			x_read_count += depth
+		elif nchromosome == 24:
+			y_read_count += depth
+		
+		minimum_autosomal_read_hits_for_sex_determination = 50
+		female_threshold = 0.01
+		male_threshold = 0.05
+		sex = 'U'
+		if autosome_read_count >= minimum_autosomal_read_hits_for_sex_determination:
+			if float(y_read_count) / autosome_read_count < female_threshold:
+				sex = 'F'
+			elif float(y_read_count) / autosome_read_count >= male_threshold:
+				sex = 'M'
 
 	coverage = len(targets) / number_of_targets
-	print("%s\t%s\t%.3f" % (key, label, coverage) )
+	print("%s\t%s\t%.3f\t%s\t%d\t%s\t%d\t%s\t%d\t%s\t%s" % (key, label + '_target_coverage', coverage, 
+													  label + '_autosome', autosome_read_count,
+													  label + '_x', x_read_count, 
+													  label + '_y', y_read_count,
+													  label + 'sex', sex) 
+	)
