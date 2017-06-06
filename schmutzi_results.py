9e153ae0e543a2ba6e40c266dc398bf96192cee3
Matthew Mah <mmah@broadinstitute.org>
2017-06-06 13:38:33 -0400

    parsing schmutzi contamination results, with default nan values

---
+++ schmutzi_results.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+key = sys.argv[1]
+filename = sys.argv[2]
+
+contamination = float('nan')
+lower = float('nan')
+upper = float('nan')
+
+try:
+	f = open(filename,'r')
+	line = f.readline()
+	fields = line.split()
+	
+	contamination = float(fields[0])
+	lower = float(fields[1])
+	upper = float(fields[2])
+	f.close()
+except:
+	pass
+finally:
+	print('%s\t%s\t%f\t%s\t%f\t%s\t%f' % (key, 'contamination', contamination,
+					   'contamination_lower', lower, 
+					   'contamination_upper', upper)
+	   )
