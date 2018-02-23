4ec3704285194f77ec61fd3b90bad7ada1795e02
Matthew Mah <mmah@broadinstitute.org>
2018-02-23 11:26:19 -0500

    Rename variables to be accurate based on histogram of reads hitting targets leading to estimate of unique reads. Use argparse.

--- preseq_process.py
+++ preseq_process.py
@@ -2,13 +2,14 @@
 # 
 
 import sys
+import argparse
 
 # raw_reads 
 # number of distinct targets hit
-def preseq_analysis(target_reads, unique_targets_uncorrected, number_raw_reads, total_target_hits_actual, unique_targets_actual, minimum_raw_reads, corrected_targets_per_raw_read_threshold):
-	length = len(target_reads)
-	if length != len(unique_targets_uncorrected):
-		raise ValueError('length mismatch between entries in target_reads and unique_targets_uncorrected')
+def preseq_analysis(reads_hitting_any_target, unique_reads, number_raw_reads, total_reads_hitting_any_target_actual, unique_targets_hit, minimum_raw_reads, expected_targets_per_raw_read_threshold):
+	length = len(reads_hitting_any_target)
+	if length != len(unique_reads):
+		raise ValueError('length mismatch between entries in reads_hitting_any_target and unique_reads')
 	
 	inverse_e = 0.3678794
 	tenth = 0.1
@@ -16,25 +17,26 @@ def preseq_analysis(target_reads, unique_targets_uncorrected, number_raw_reads, 
 	reads_inverse_e = float('inf')
 	reads_tenth = float('inf')
 	total_reads_required = minimum_raw_reads
-	expected_unique_targets_at_saturation = -1
+	expected_unique_targets_at_threshold = -1
 	
 	# not all demultiplexing reads merge, align, or map to targets
 	# use a simple ratio to translate between reads for preseq and demultiplexing reads
-	read_ratio = float(number_raw_reads) / total_target_hits_actual
+	read_ratio = float(number_raw_reads) / total_reads_hitting_any_target_actual
+	#print(read_ratio)
 	
 	for i in range(1,length):
-		slope_uncorrected_targets = float(unique_targets_uncorrected[i] - unique_targets_uncorrected[i-1]) / (target_reads[i] - target_reads[i-1])
-		if slope_uncorrected_targets <= inverse_e:
-			reads_inverse_e = min(reads_inverse_e, target_reads[i])
-		if slope_uncorrected_targets <= tenth:
-			reads_tenth = min(reads_tenth, target_reads[i])
+		#slope_uncorrected_targets = float(unique_reads[i] - unique_reads[i-1]) / (reads_hitting_any_target[i] - reads_hitting_any_target[i-1])
 			
-		slope_corrected_targets = (target_hits_estimated_to_empirical(unique_targets_uncorrected[i]) - target_hits_estimated_to_empirical(unique_targets_uncorrected[i-1])) / ((target_reads[i] - target_reads[i-1]) * read_ratio)
-		#print("{:f}\t{:f}\t{:f}\t{:f}".format(target_reads[i], target_reads[i] * read_ratio, unique_targets_uncorrected[i], slope_corrected_targets))
+		slope_corrected_targets = (target_hits_estimated_to_empirical(unique_reads[i]) - target_hits_estimated_to_empirical(unique_reads[i-1])) / ((reads_hitting_any_target[i] - reads_hitting_any_target[i-1]) * read_ratio)
+		#print("{:f}\t{:f}\t{:f}\t{:f}".format(reads_hitting_any_target[i], reads_hitting_any_target[i] * read_ratio, unique_reads[i], slope_corrected_targets))
+		if slope_corrected_targets <= inverse_e:
+			reads_inverse_e = min(reads_inverse_e, reads_hitting_any_target[i])
+		if slope_corrected_targets <= tenth:
+			reads_tenth = min(reads_tenth, reads_hitting_any_target[i])
 		
-		if slope_corrected_targets > corrected_targets_per_raw_read_threshold:
-			total_reads_required = max(total_reads_required, target_reads[i] * read_ratio)
-			expected_unique_targets_at_saturation = target_hits_estimated_to_empirical(unique_targets_uncorrected[i])
+		if slope_corrected_targets > expected_targets_per_raw_read_threshold:
+			total_reads_required = max(total_reads_required, reads_hitting_any_target[i] * read_ratio)
+			expected_unique_targets_at_threshold = target_hits_estimated_to_empirical(unique_reads[i])
 		
 			
 	raw_reads_inverse_e = reads_inverse_e * read_ratio
@@ -43,18 +45,20 @@ def preseq_analysis(target_reads, unique_targets_uncorrected, number_raw_reads, 
 	
 	values = {
 		'number_raw_reads': number_raw_reads,
-		'unique_targets_actual': unique_targets_actual,
+		'unique_targets_hit': unique_targets_hit,
 		'raw_reads_inverse_e': raw_reads_inverse_e,
 		'raw_reads_tenth': raw_reads_tenth,
 		'total_reads_required': total_reads_required,
 		'additional_reads_required': additional_reads_required,
-		'expected_unique_targets_at_saturation': expected_unique_targets_at_saturation
+		'expected_unique_targets_at_threshold': expected_unique_targets_at_threshold
 		}
 	return values
 
 # count number of reads overlapping any target and number of unique targets hit
-def total_and_unique_target_hits(histogram_filename):
-	with open(histogram_filename) as histogram:
+# This histogram is not the input to preseq. It counts the number of reads
+# for each target, which allows counting the actual number of targets hit
+def total_and_unique_target_hits(target_histogram_filename):
+	with open(target_histogram_filename) as histogram:
 		total_hits = 0
 		unique_targets = 0
 		for line in histogram:
@@ -67,33 +71,38 @@ def total_and_unique_target_hits(histogram_filename):
 def target_hits_estimated_to_empirical(estimated_hits):
 	total_autosome_targets = 1150639 # autosome targets in the 1240k target set
 	estimated_hit_fraction = max(float(estimated_hits) / total_autosome_targets, 1e-9)
-	corrected_hit_faction = 1 / (1.1 + 1 / estimated_hit_fraction)
+	corrected_hit_faction = 1 / (1 + 1 / estimated_hit_fraction)
 	corrected_hit_estimate = corrected_hit_faction * total_autosome_targets
 	return corrected_hit_estimate
 
 if __name__ == '__main__':
-	histogram_filename = sys.argv[1]
-	# empirically measured parameters to extrapolate from
-	total_target_hits_actual, unique_targets_actual = total_and_unique_target_hits(histogram_filename)
+	parser = argparse.ArgumentParser()
+	# number of raw demultiplexing reads that produced the histogram processed by preseq
+	parser.add_argument("-n", "--number_raw_reads", help="number of raw reads demultiplexing", type=int, required=True)
+	# minimum raw reads required for a library
+	parser.add_argument("-m", "--minimum_raw_reads", help="minimum required number of raw reads demultiplexing", type=int, default=3e6)
+	parser.add_argument("-t", "--expected_targets_per_raw_read_threshold", help="threshold for ratio of expected unique targets hit to raw demultiplexing reads", type=float, default=0.01)
 	
-	preseq_filename = sys.argv[2]
-	number_raw_reads = int(sys.argv[3]) # number of raw reads that produced the histogram processed by preseq
-	minimum_raw_reads = int(sys.argv[4]) # minimum raw reads required for a library
-	corrected_targets_per_raw_read_threshold = float(sys.argv[5])
+	parser.add_argument("target_histogram_filename", help="Target histogram to count number of actual target hits. This is not the input to preseq.")
+	parser.add_argument("preseq_filename", help="preseq file containing mapping of reads hitting targets to unique reads hitting targets")
+	args = parser.parse_args()
+	
+	# empirically measured parameters to extrapolate from
+	total_reads_hitting_any_target_actual, unique_targets_hit = total_and_unique_target_hits(args.target_histogram_filename)
 
-	target_reads = []
-	unique_targets_uncorrected = []
-	with open(preseq_filename) as preseq_file:
+	reads_hitting_any_target = []
+	unique_reads = []
+	with open(args.preseq_filename) as preseq_file:
 		# skip preseq header
 		preseq_file.readline()
 		
 		# read in table of reads
 		for line in preseq_file:
 			reads, expected_distinct_reads, lower, upper = line.split()
-			target_reads.append(float(reads))
-			unique_targets_uncorrected.append(float(expected_distinct_reads))
+			reads_hitting_any_target.append(float(reads))
+			unique_reads.append(float(expected_distinct_reads))
 			
-	values = preseq_analysis(target_reads, unique_targets_uncorrected, number_raw_reads, total_target_hits_actual, unique_targets_actual, minimum_raw_reads, corrected_targets_per_raw_read_threshold)
+	values = preseq_analysis(reads_hitting_any_target, unique_reads, args.number_raw_reads, total_reads_hitting_any_target_actual, unique_targets_hit, args.minimum_raw_reads, args.expected_targets_per_raw_read_threshold)
 	for key in values:
 		print('{}\t{:.1f}'.format(key, values[key]))
 
