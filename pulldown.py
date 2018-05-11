3d85aa755afac99b51a118bc0884c62aeb3752e2
Matthew Mah <mmah@broadinstitute.org>
2018-05-11 18:15:50 -0400

    python script now generates pulldown files

--- pulldown.py
+++ pulldown.py
@@ -63,18 +63,22 @@ def damage_restricted_d(string):
 	return "{}_d".format(string)
 
 def create_individual_file(filename, library_parameters, udg_filter, id_transform_function, sex_by_index_barcode_key):
+	count = 0
 	with open(filename, 'w') as individual_file:
 		for parameters in library_parameters:
 			if parameters.udg == udg_filter:
+				count += 1
 				library_id = id_transform_function(parameters.library_id)
 				sex = sex_by_index_barcode_key[parameters.index_barcode_key]
-				individual_file.write("{0}\t{1}\t{0}".format(library_id, sex))
-	return filename
+				individual_file.write("{0}\t{1}\t{0}\n".format(library_id, sex))
+	return count
 
-pulldown_options = {'normal' : non_damage_restricted_options, 'damage_restricted' : damage_restricted_options}
-def create_pulldown_parameter_file(pulldown_label, pulldown_type):
-	pulldown_string = "{}.{}".format(pulldown_label, pulldown_type)
-	pulldown_parameters = pulldown_parameters_base.replace('LABEL', pulldown_label).replace('PULLDOWN_STRING', pulldown_string) + pulldown_options[pulldown_type]
+NORMAL = 'normal'
+DAMAGE_RESTRICTED = 'damage_restricted'
+pulldown_damage_options = {NORMAL : non_damage_restricted_options, DAMAGE_RESTRICTED : damage_restricted_options}
+def create_pulldown_parameter_file(pulldown_label, pulldown, udg_type, damage_type):
+	pulldown_string = "{}.{}.{}".format(pulldown_label, udg_type, damage_type)
+	pulldown_parameters = pulldown_parameters_base.replace('LABEL', pulldown_label).replace('UDG', udg_type).replace('PULLDOWN_STRING', pulldown_string) + pulldown_damage_options[damage_type]
 	pulldown_parameter_filename_nopath = "{}.parameters".format(pulldown_string)
 	with open("{}".format(pulldown_parameter_filename_nopath), 'w') as pulldown_parameter_file:
 		pulldown_parameter_file.write(pulldown_parameters)
@@ -91,49 +95,38 @@ def prepare_pulldown(library_parameters, args):
 	PLUS = 'plus'
 	ALLOWED_UDG_VALUES = [MINUS, HALF, PLUS]
 	
-	# Generate individual files
-	# normal pulldown for udg half individual file
-	create_individual_file("{}.half.normal.ind".format(pulldown_label), library_parameters, HALF, identity, sex_by_index_barcode_key)
-	# damage restricted pulldown for udg half individual file
-	create_individual_file("{}.half.damage_restricted.ind".format(pulldown_label), library_parameters, HALF, damage_restricted_d, sex_by_index_barcode_key)
-	
-	# normal pulldown for udg minus individual file
-	create_individual_file("{}.minus.normal.ind".format(pulldown_label), library_parameters, MINUS, identity, sex_by_index_barcode_key)
-	# damage restricted pulldown for udg minus individual file
-	create_individual_file("{}.minus.damage_restricted.ind".format(pulldown_label), library_parameters, MINUS, damage_restricted_d, sex_by_index_barcode_key)
-	
-	# UDG plus cannot be damage restricted
-	create_individual_file("{}.plus.normal.ind".format(pulldown_label), library_parameters, PLUS, identity, sex_by_index_barcode_key)
-	
+	# check that all UDG values are allowed
 	for parameters in library_parameters:
 		if parameters.udg not in ALLOWED_UDG_VALUES:
 			raise ValueError('Unhandled udg {}'.format(parameters.udg))
+	
+	parameter_indices = [
+		(HALF, NORMAL),
+		(HALF, DAMAGE_RESTRICTED),
+		(MINUS, NORMAL),
+		(MINUS, DAMAGE_RESTRICTED),
+		(PLUS, NORMAL) # UDG plus cannot be damage restricted
+		]
+	for (udg_type, damage_type) in parameter_indices:
+		pulldown_base_filename = "{}.{}.{}".format(pulldown_label, udg_type, damage_type)
+		# Generate individual files
+		name_manipulation_function = identity if damage_type == NORMAL else damage_restricted_d
+		create_individual_file(pulldown_base_filename + ".ind", library_parameters, udg_type, name_manipulation_function, sex_by_index_barcode_key)
+		# build parameter files
+		create_pulldown_parameter_file(pulldown_label, udg_type, damage_type)
 			
 	# build Nick-style database file
 	current_directory = os.getcwd()
 	with open("{}.dblist".format(pulldown_label), 'w') as db_file:
 		for parameters in library_parameters:
 			# filename must match the deduplicated bam
-			release_library_name = library_parameters.get_release_library_name()
+			release_library_name = parameters.get_release_library_name()
 			
 			library_bam_filename  = "{}/{}/{}".format(args.release_directory, parameters.library_id, release_library_name)
-			db_line = "{0}\t{0}\t{1}\t{2}".format(library_id, library_bam_filename, ":".join(read_groups))
+			read_groups = read_groups_from_bam(library_bam_filename)
+			db_line = "{0}\t{0}\t{1}\t{2}".format(parameters.library_id, library_bam_filename, ":".join(read_groups))
 			db_file.write(db_line)
-			
-	# build parameter files
-	pulldown_options = {'normal' : non_damage_restricted_options, 'damage_restricted' : damage_restricted_options}
-	for pulldown_type in pulldown_options:
-		pulldown_string = "{}.{}".format(pulldown_label, pulldown_type)
-		pulldown_parameters = pulldown_parameters_base.replace('LABEL', pulldown_label).replace('PULLDOWN_STRING', pulldown_string) + pulldown_options[pulldown_type]
-		pulldown_parameter_filename_nopath = "{}.parameters".format(pulldown_string)
-		with open("{}".format(pulldown_parameter_filename_nopath), 'w') as pulldown_parameter_file:
-			pulldown_parameter_file.write(pulldown_parameters)
-		
-	# damage restricted
-	damage_restricted_parameters = pulldown_parameters_library.replace('SAMPLE_MACRO', library_id + '_d') + damage_restricted_options
-	pulldown_parameter_damage_restricted_filename_nopath = "{}.damage_restricted.parameters".format(library_id)
-	with open("{}/{}".format(working_directory, pulldown_parameter_damage_restricted_filename_nopath), 'w') as pulldown_parameter_file:
-		pulldown_parameter_file.write(damage_restricted_parameters)
+	
 
 def pulldown(pulldown_parameter_filename):
 	with open('{}.stdout'.format(pulldown_parameter_filename), 'w') as stdout_pulldown, \
