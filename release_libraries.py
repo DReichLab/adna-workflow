0726658cd1a8722d97318874c8773df0ad8f90f4
Matthew Mah <mmah@broadinstitute.org>
2018-12-07 09:38:05 -0500

    remove duplicates in release libraries

--- release_libraries.py
+++ release_libraries.py
@@ -51,8 +51,8 @@ def build_release_library(adna_jar_filename, picard_jar, working_directory, libr
 			library_with_duplicates_filename = "{0}.{1}.{2}.duplicates.bam".format(library_id, experiment, reference)
 			subprocess.run("java -Xmx5500m -jar {} MergeSamFiles I={} O={} SORT_ORDER=coordinate".format(picard_jar, ' I='.join(library_component_bams), library_with_duplicates_filename), shell=True, check=True, cwd=working_directory, stdout=stdout_build, stderr=stderr_build)
 			
-			# mark, but leave duplicates
-			subprocess.run("java -Xmx5500m -jar {0} MarkDuplicates I={1} O={2} M={2}.dedup_stats BARCODE_TAG=XD ADD_PG_TAG_TO_READS=false MAX_FILE_HANDLES=1000".format(picard_jar, library_with_duplicates_filename, library_filename), shell=True, check=True, cwd=working_directory, stdout=stdout_build, stderr=stderr_build)
+			# deduplicate
+			subprocess.run("java -Xmx5500m -jar {0} MarkDuplicates I={1} O={2} M={2}.dedup_stats REMOVE_DUPLICATES=true BARCODE_TAG=XD ADD_PG_TAG_TO_READS=false MAX_FILE_HANDLES=1000".format(picard_jar, library_with_duplicates_filename, library_filename), shell=True, check=True, cwd=working_directory, stdout=stdout_build, stderr=stderr_build)
 		else: # There are no reads, so use an empty bam. First bam should exist and be empty, so return a copy of that
 			shutil.copy(library_parameters.bam_filenames[0], working_directory + '/' + library_filename)
 		
