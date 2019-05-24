6b993a09b1a6d1148c70ff50e9bdc6f76c9294ed
Matthew Mah <mmah@broadinstitute.org>
2019-05-24 12:08:54 -0400

    Process multiple bamlists for release cleaning at once

--- clean_release.py
+++ clean_release.py
@@ -10,19 +10,21 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Move released bam files corresponding to a release list. This is to ensure they get replaced.")
 	parser.add_argument("-p", "--parent", help="parent directory for released files", default="/n/groups/reich/matt/pipeline/released_libraries")
 	parser.add_argument("-d", "--directory", help="destination directory to move files", required=True)
-	parser.add_argument("release_list", help="bamlist used for assembling bams")
+	parser.add_argument("release_list", help="bamlist used for assembling bams", nargs='+')
 	
 	args = parser.parse_args()
 	
-	filename = args.release_list
+	bamlists = args.release_list
 
 	instance_ids = set()
 	parent_path = Path(args.parent)
 	
-	with open(filename) as f:
-		library_parameters = [LibraryParameters(line) for line in f]
+	for bamlist in bamlists:
+		with open(bamlist) as f:
+			library_parameters = [LibraryParameters(line) for line in f]
 	
-	for library in library_parameters:
-		source = library.get_release_library_path(args.parent)
-		if os.path.exists(source):
-			shutil.move(source, args.directory)
+		for library in library_parameters:
+			source = library.get_release_library_path(args.parent)
+			print(source)
+			if os.path.exists(source):
+				shutil.move(source, args.directory)
