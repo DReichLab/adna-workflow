43e34a93caccb0001e463f58fdcd80caf8ffeef7
Matthew Mah <mmah@broadinstitute.org>
2019-07-19 12:47:40 -0400

    Script to test whether BAM has XD tag used for deduplication

---
+++ duplicates_tags.py
@@ -0,0 +1,22 @@
+import pysam
+import argparse
+
+# test first read to see whether it has an XD tag
+def bam_has_XD_tag(filename):
+	tag_contents = None
+	samfile = pysam.AlignmentFile(args.bam, "rb")
+	for read in samfile:
+		if read.has_tag('XD'):
+			tag_contents = read.get_tag('XD')
+		break
+	samfile.close()
+	#print(tag_contents)
+	return tag_contents is not None
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Test whether a BAM file has XD tags used for barcode-aware deduplication by the Reich Lab pipeline software")
+	
+	parser.add_argument("bam", help="BAM file to inspect for XD tag")
+	args = parser.parse_args()
+	result = bam_has_XD_tag(args.bam)
+	print(result)
