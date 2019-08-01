78020aa6f9493088c822d3608473cfe5e8afd254
Matthew Mah <mmah@broadinstitute.org>
2019-08-01 14:22:18 -0400

    Augment the bam list for a release with a prior existing version of the library

---
+++ prior_library_release.py
@@ -0,0 +1,28 @@
+from release_libraries import LibraryParameters
+from bam_finder import getBamPath
+import argparse
+import re
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Augment the bam list for a release with a prior existing version of the library")
+	parser.add_argument("bam_list", help="Each line contains the parameters to build a library bam for release. This includes the library ID, the individual ID, experiment, read group description (sequencing run name with experiment type and udg treatment), experiment, and (bam, sequencing run date) pairs ")
+	args = parser.parse_args()
+
+	with open(args.bam_list) as f:
+		library_parameters = [LibraryParameters(line) for line in f]
+
+	for x in library_parameters:
+		experiment = x.experiment
+		if '1240k' in experiment:
+			experiment = '1240k'
+		existingBAM = getBamPath(x.library_id, experiment=experiment, reference=x.reference, version_policy='latest')
+		bam = str(existingBAM)
+		if len(bam) > 0:
+			match = re.search('v([0-9]+).bam', bam)
+			new_version = int(match.group(1)) + 1
+			#print('{}\t{}\t{:d}'.format(x.library_id, bam, new_version))
+			x.version = new_version
+			x.bam_filenames.append(str(existingBAM))
+			x.bam_date_strings.append('noDate') # the bam date string is used for generating read groups, which the existing bam does not need
+		#print('{}\t{}'.format(x.library_id, bam))
+		print(x)
