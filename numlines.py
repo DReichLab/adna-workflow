f3ecdd138f5faecbc8f3d34dc1cba5165ee07f18
Matthew Mah <mmah@broadinstitute.org>
2019-10-07 11:16:25 -0400

    simple utility to read first n lines of file or piped input

---
+++ numlines.py
@@ -0,0 +1,12 @@
+import argparse
+import sys
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Print the desired number of lines from a file")
+	parser.add_argument('-n', "--num_lines", help="number of lines to output", default=10, type=int)
+	parser.add_argument('inputfile', nargs='?', type=argparse.FileType('r'))
+	args = parser.parse_args() 
+	
+	#with open(args.inputfile) as f:
+	for n in range(args.num_lines):
+		print(args.inputfile.readline(), end='')
