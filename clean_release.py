3e7fd8f160b4858ddf80ff37a6a2b61b0b295c32
Matthew Mah <mmah@broadinstitute.org>
2019-10-16 16:12:42 -0400

    option to specify maximum age to copy when cleaning releases

--- clean_release.py
+++ clean_release.py
@@ -3,6 +3,7 @@ import argparse
 import shutil
 import os
 from pathlib import Path
+import datetime
 
 from release_libraries import LibraryParameters
 
@@ -10,11 +11,15 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Move released bam files corresponding to a release list. This is to ensure they get replaced.")
 	parser.add_argument("-p", "--parent", help="parent directory for released files", default="/n/groups/reich/matt/pipeline/released_libraries")
 	parser.add_argument("-d", "--directory", help="destination directory to move files", required=True)
+	parser.add_argument("-a", "--age", help="Maximum age of files to move in days. ", type=int)
 	parser.add_argument("release_list", help="bamlist used for assembling bams", nargs='+')
 	
 	args = parser.parse_args()
 	
 	bamlists = args.release_list
+	if args.age:
+		allowed_age = datetime.timedelta(days=args.age)
+	now = datetime.datetime.now()
 
 	instance_ids = set()
 	parent_path = Path(args.parent)
@@ -27,4 +32,11 @@ if __name__ == "__main__":
 			source = '{}/{}'.format(library.get_release_library_path(args.parent), library.get_release_library_name())
 			print(source)
 			if os.path.exists(source):
-				shutil.move(source, args.directory)
+				if args.age is None:
+					shutil.move(source, args.directory)
+				else:
+					modified = datetime.datetime.fromtimestamp(os.stat(source).st_mtime)
+					if (now - modified) < allowed_age: 
+						shutil.move(source, args.directory)
+					else:
+						print('{} is older than requested age', file=sys.stderr)
