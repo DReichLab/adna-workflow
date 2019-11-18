e67ea000e69bbd2e3a7e340e83bcc94d9c2c92a8
Matthew Mah <mmah@broadinstitute.org>
2019-11-18 16:16:56 -0500

    memory options for release libraries because large shotgun bams need more memory

--- release_libraries.py
+++ release_libraries.py
@@ -15,8 +15,8 @@ def bam_has_reads(bam_filename):
 	result = subprocess.run(['samtools', 'view', '-c', bam_filename], check=True, universal_newlines=True, stdout=subprocess.PIPE).stdout.strip()
 	return int(result) > 0
 
-def add_read_groups(adna_jar_filename, demultiplexed_bam_filename, output_bam_filename, bam_date_string, label, library_id, individual, working_directory):
-	subprocess.run(["java", "-Xmx5000m", "-jar", adna_jar_filename,
+def add_read_groups(adna_jar_filename, demultiplexed_bam_filename, output_bam_filename, bam_date_string, label, library_id, individual, working_directory, jvm_mem_string):
+	subprocess.run(["java", jvm_mem_string, "-jar", adna_jar_filename,
 				 "AssignReadGroups", 
 				 "-i", demultiplexed_bam_filename,
 				 "-o", output_bam_filename,
@@ -25,7 +25,7 @@ def add_read_groups(adna_jar_filename, demultiplexed_bam_filename, output_bam_fi
 				 "-d", bam_date_string,
 				 "-l", library_id], check=True, cwd=working_directory)
 
-def build_release_library(adna_jar_filename, picard_jar, working_directory, library_parameters):
+def build_release_library(adna_jar_filename, picard_jar, working_directory, library_parameters, jvm_mem_string):
 	# make a working directory for this library
 	pathlib.Path(working_directory).mkdir(exist_ok=True)
 	
@@ -49,13 +49,13 @@ def build_release_library(adna_jar_filename, picard_jar, working_directory, libr
 			if not has_read_groups:
 				bam_date_string = library_parameters.bam_date_strings[i]
 				label = "{}_{}".format(library_parameters.read_group_description, library_id)
-				add_read_groups(adna_jar_filename, component_bam_filename, output_bam_filename, bam_date_string, label, library_id, library_parameters.individual_id, working_directory)
+				add_read_groups(adna_jar_filename, component_bam_filename, output_bam_filename, bam_date_string, label, library_id, library_parameters.individual_id, working_directory, jvm_mem_string)
 			# Shop's bams need read groups rewritten
 			elif not has_real_library_name:
 				shop_version = ShopVersion(component_bam_filename)
 				bam_date_string = shop_version.date_string
 				label = "{}_{}".format(library_parameters.read_group_description, library_id)
-				add_read_groups(adna_jar_filename, component_bam_filename, output_bam_filename, bam_date_string, label, library_id, library_parameters.individual_id, working_directory)
+				add_read_groups(adna_jar_filename, component_bam_filename, output_bam_filename, bam_date_string, label, library_id, library_parameters.individual_id, working_directory, jvm_mem_string)
 			# Previously released libraries already have read groups, and do not need them added
 			# Simply include these in the merge
 			else:
@@ -70,13 +70,13 @@ def build_release_library(adna_jar_filename, picard_jar, working_directory, libr
 		if len(library_component_bams) > 0: # merge any bams with reads and mark duplicates
 			# merge 
 			library_with_duplicates_filename = "{0}.{1}.{2}.duplicates.bam".format(library_id, experiment, reference)
-			subprocess.run("java -Xmx5500m -jar {} MergeSamFiles I={} O={} SORT_ORDER=coordinate".format(picard_jar, ' I='.join(library_component_bams), library_with_duplicates_filename), shell=True, check=True, cwd=working_directory, stdout=stdout_build, stderr=stderr_build)
+			subprocess.run("java {} -jar {} MergeSamFiles I={} O={} SORT_ORDER=coordinate".format(jvm_mem_string, picard_jar, ' I='.join(library_component_bams), library_with_duplicates_filename), shell=True, check=True, cwd=working_directory, stdout=stdout_build, stderr=stderr_build)
 			
 			# if any component bam is missing the XD tag, we cannot use the tag to deduplicate properly with barcodes
 			# We treat this conservatively by removing all barcode information and deduplicating based on position and length
 			if component_bam_missing_duplicate_tag:
 				library_with_duplicates_tag_rewritten_filename = "{0}.{1}.{2}.duplicates.tagxd.bam".format(library_id, experiment, reference)
-				subprocess.run(['java', '-Xmx5500m', '-jar', adna_jar_filename, 'DuplicatesTagRewrite', 
+				subprocess.run(['java', jvm_mem_string, '-jar', adna_jar_filename, 'DuplicatesTagRewrite', 
 					'-i', library_with_duplicates_filename,
 					'-o', library_with_duplicates_tag_rewritten_filename], check=True, cwd=working_directory)
 				to_deduplicate_filename = library_with_duplicates_tag_rewritten_filename
@@ -84,7 +84,7 @@ def build_release_library(adna_jar_filename, picard_jar, working_directory, libr
 				to_deduplicate_filename = library_with_duplicates_filename
 			
 			# deduplicate
-			subprocess.run("java -Xmx5500m -jar {0} MarkDuplicates I={1} O={2} M={2}.dedup_stats REMOVE_DUPLICATES=true BARCODE_TAG=XD ADD_PG_TAG_TO_READS=false MAX_FILE_HANDLES=1000".format(picard_jar, to_deduplicate_filename, library_filename), shell=True, check=True, cwd=working_directory, stdout=stdout_build, stderr=stderr_build)
+			subprocess.run("java {0} -jar {1} MarkDuplicates I={2} O={3} M={3}.dedup_stats REMOVE_DUPLICATES=true BARCODE_TAG=XD ADD_PG_TAG_TO_READS=false MAX_FILE_HANDLES=1000".format(jvm_mem_string, picard_jar, to_deduplicate_filename, library_filename), shell=True, check=True, cwd=working_directory, stdout=stdout_build, stderr=stderr_build)
 		else: # There are no reads, so use an empty bam. First bam should exist and be empty, so return a copy of that
 			shutil.copy(library_parameters.bam_filenames[0], working_directory + '/' + library_filename)
 		
@@ -152,10 +152,10 @@ class LibraryParameters:
 		release_library_path = "{}/{}".format(release_directory, self.library_id)
 		return release_library_path
 	
-def process_library(adna_jar_filename, picard_jar, release_directory, library_parameters):
+def process_library(adna_jar_filename, picard_jar, release_directory, library_parameters, jvm_mem_string):
 	working_directory = library_parameters.library_id
 	
-	release_library_filename = build_release_library(adna_jar_filename, picard_jar, working_directory, library_parameters)
+	release_library_filename = build_release_library(adna_jar_filename, picard_jar, working_directory, library_parameters, jvm_mem_string)
 	copy_release_library(library_parameters, release_directory, release_library_filename, working_directory)
 	index_library(library_parameters, release_directory, release_library_filename)
 		
@@ -164,6 +164,7 @@ if __name__ == "__main__":
 	
 	parser.add_argument('-n', "--num_threads", help="size of thread pool", type=int, default =10)
 	parser.add_argument('-d', "--release_directory", help="parent directory to put released libraries")
+	parser.add_argument('-m', "--mem", help='memory string for java virtual machine, for example "5000m" or "10G"', default='5500m')
 	
 	parser.add_argument("bam_list", help="Each line contains the parameters to build a library bam for release. This includes the library ID, the individual ID, experiment, read group description (sequencing run name with experiment type and udg treatment), experiment, and (bam, sequencing run date) pairs ")
 	parser.add_argument("adna_jar", help="jar file for assigning read groups to each read in a bam")
@@ -177,9 +178,10 @@ if __name__ == "__main__":
 	# build libraries
 	adna_jar_filename = args.adna_jar
 	picard_jar = args.picard_jar
+	jvm_mem_string = '-Xmx{}'.format(args.mem)
 	
 	pool = Pool(processes=args.num_threads)
-	results = [pool.apply_async(process_library, args=(adna_jar_filename, picard_jar, args.release_directory, parameters)) for parameters in library_parameters]
+	results = [pool.apply_async(process_library, args=(adna_jar_filename, picard_jar, args.release_directory, parameters, jvm_mem_string)) for parameters in library_parameters]
 	pool.close()
 	[result.get() for result in results] # check for exceptions
 	pool.join()
