dc5f3575dec36f8271180d4a0c1a06c652837e14
Matthew Mah <mmah@broadinstitute.org>
2020-07-01 16:29:44 -0400

    Allow multiple histogram inputs for histogram_counts

--- histogram_counts.py
+++ histogram_counts.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 
 from collections import Counter
+from math import sqrt
 from random import random, seed
 
 def load_histogram(target_histogram_filename):
@@ -45,20 +46,31 @@ def total_and_unique_target_hits(counter, retain_probability=1.0):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="For analysis of histograms associated with preseq. From the histogram of the number of appearances of each read, report total hits and unique reads. Options relate to downsampling for computing preseq-like marginal uniquness computation.")
 	parser.add_argument("-f", "--fraction", type=float, default=1.0, help='For downsampling, the fraction of reads that will be retained.')
-	parser.add_argument("histogram_filename", help="preseq input histogram")
+	parser.add_argument("histogram_filename", help="preseq input histogram", nargs='+')
 	parser.add_argument("-s", "--seed", help="random number seed for downsampling randomization", default=2020)
 	parser.add_argument("-n", "--iterations", type=int, default=1, help='Number of iterations to average counts of reads and unique reads when downsampling')
 	args = parser.parse_args()
 	
-	seed(args.seed)
-	#print(retained_copies(args.example, args.fraction))
-	counter = load_histogram(args.histogram_filename)
-	total_hits = 0
-	unique_targets = 0
-	for i in range(args.iterations):
-		interation_hits, iteration_unique_targets = total_and_unique_target_hits(counter, args.fraction)
-		total_hits += interation_hits
-		unique_targets += iteration_unique_targets
-	average_total_hits = total_hits / args.iterations
-	average_unique_targets = unique_targets / args.iterations
-	print(average_total_hits, average_unique_targets)
+	for histogram_filename in args.histogram_filename:
+		seed(args.seed)
+		counter = load_histogram(histogram_filename)
+		total_hits = 0
+		unique_targets = 0
+		unique_targets_squared = 0
+		for i in range(args.iterations):
+			interation_hits, iteration_unique_targets = total_and_unique_target_hits(counter, args.fraction)
+			total_hits += interation_hits
+			unique_targets += iteration_unique_targets
+			unique_targets_squared += iteration_unique_targets ** 2
+		average_total_hits = total_hits / args.iterations
+		average_unique_targets = unique_targets / args.iterations
+		variance = unique_targets_squared / args.iterations - average_unique_targets ** 2
+		standard_deviation = sqrt(variance)
+		lower_interval = average_unique_targets - 1.96 * standard_deviation
+		upper_interval = average_unique_targets + 1.96 * standard_deviation
+		
+		if args.iterations > 1:
+			interval = '{:.3f}\t{:.3f}'.format(lower_interval, upper_interval)
+			print(f'{average_total_hits}\t{average_unique_targets}\t{interval}')
+		else:
+			print(f'{average_total_hits}\t{average_unique_targets}')
